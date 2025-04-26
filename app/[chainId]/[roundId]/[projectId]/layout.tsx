9cf08c578e5886a163461e47ef028b095e223150
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-04-26 08:23:39 +0200

    Add Project Layout

---
+++ app/[chainId]/[roundId]/[projectId]/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import { fetchApplicationById } from "@/hooks/useApplications";
+
+type Props = {
+  params: { projectId: string; chainId: string; roundId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const project = await fetchApplicationById({
+    id: params.projectId,
+    chainId: params.chainId,
+  });
+
+  if (!project) {
+    return {
+      title: "Project Not Found",
+      description: "The requested project could not be found.",
+    };
+  }
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: project.name,
+  };
+}
+
+export default function ProjectLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
